0380eb1eca59125d1355190fef6ba77c19ff6c84
humanafterall2023 <133306177+humanafterall2023@users.noreply.github.com>
2023-05-17 09:46:37 -0700

    Update route.ts
    Fix typescript error

--- app/api/create_image/route.ts
+++ app/api/create_image/route.ts
@@ -4,6 +4,9 @@ import { sendEmail, addUserToList } from "@/lib/mail";
 
 export async function POST(request: Request) {
   const result = await createImage(await request.json());
+  if (!result) return NextResponse.json({ 
+    error: "Error generating image",
+  });
   await sendEmail(result);
   await addUserToList(result.userEmail);
   return NextResponse.json(result);
